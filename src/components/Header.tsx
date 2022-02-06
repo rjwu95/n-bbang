3e92d301ce1958a51e7d4e15ff48976b750f52cd
Gun Yu <yugun000@gmail.com>
2022-02-06 22:17:50 +0900

    set up bootstrap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./Header.css";
 
 const Header = () => (
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/users">Users</Link>
-      </li>
-    </ul>
-  </nav>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="/">N빵</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#search">검색</Nav.Link>
+          {/* <Nav.Link href="/">Home</Nav.Link> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 export default Header;
